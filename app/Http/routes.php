843e9b127c8dabd1fc019c52baf36e6b5321e40c
Matthias Kuhs <matthiku@gmail.com>
2015-09-21 11:53:13 +0100

    Adding Controllers
    controllers for every route to our API using only the necessary methods

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,5 +11,9 @@
 |
 */
 
-Route::get('/{name?}', 'MyController@index');
+Route::resource('makers', 'MakerController', ['except' => ['create','edit']]);
+
+Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
+
+Route::resource('makers.vehicles', 'MakerVehiclesController', ['except' => ['edit', 'create']]);
 
